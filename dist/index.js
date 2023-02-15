fc8301b4b0c76aeeb1dfedc712ea3d108a3f44a7
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-15 14:26:08 +0300

    public & private keywords

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,31 @@
 class User {
     constructor(age, email) {
         // with a default value
-        this.name = 'John Doe'; //initializing the property
+        this.name = 'John Doe'; //initializing the property - default public
         //readonly property
         this.city = 'New York';
+        // private property - can only be accessed within the class
+        this.phone = '1234567890'; //alternate syntax - #phone: string = '1234567890';
         this.age = age;
         this.email = email;
+        this.phone;
+        // this.phone = 2345678901; //to access within the class
     }
 }
 const user = new User(30, "anne@dev.to"); // this is an instance of the class
 user.name = "Anne";
+//user.phone //error- private and can only be accessed within the class
 console.log(user);
+// Alternate class syntax
+class User2 {
+    constructor(name, age, email, phone = '1234567890', city = 'New York') {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.city = city;
+    }
+}
+const user2 = new User2("Jane Smith", 30);
+user2.email = 'jane@gmail.com';
+console.log(user2);
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,21 @@
 
 class User {
     // with a default value
-    name: string = 'John Doe';  //initializing the property
+    name: string = 'John Doe';  //initializing the property - default public
     // without a default value
-    age: number;
+    public age: number;
     // optional property
     email?: string;
     //readonly property
     readonly city: string = 'New York';
+    // private property - can only be accessed within the class
+    private phone: string = '1234567890'; //alternate syntax - #phone: string = '1234567890';
     
     constructor(age: number, email?: string) {
         this.age = age;
         this.email = email;
+        this.phone
+        // this.phone = 2345678901; //to access within the class
     }
 }
 
@@ -20,8 +24,31 @@ const user = new User(30,"anne@dev.to") // this is an instance of the class
 
 user.name = "Anne";
 
+//user.phone //error- private and can only be accessed within the class
+
+
 
 console.log(user);
+
+// Alternate class syntax
+
+class User2 {
+
+    constructor(
+        public name: string,
+        public age: number,
+        public email?: string,
+        private phone: string = '1234567890',
+        readonly city: string = 'New York'
+    ) {}
+
+}
+
+const user2 = new User2("Jane Smith", 30);
+
+user2.email = 'jane@gmail.com'
+
+console.log(user2);
 
 
 
