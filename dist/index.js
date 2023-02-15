7b77c37547408f872b924fd3f80976ed26c5a918
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-15 12:01:57 +0300

    classes in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log('Hello World!');
-console.log('Typescript is Here!');
+// creating classes in typescript
+class User {
+    constructor(age, email) {
+        // with a default value
+        this.name = 'John Doe'; //initializing the property
+        //readonly property
+        this.city = 'New York';
+        this.age = age;
+        this.email = email;
+    }
+}
+const user = new User(30, "anne@dev.to"); // this is an instance of the class
+user.name = "Anne";
+console.log(user);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-console.log('Hello World!')
-console.log('Typescript is Here!')
+// creating classes in typescript
+
+class User {
+    // with a default value
+    name: string = 'John Doe';  //initializing the property
+    // without a default value
+    age: number;
+    // optional property
+    email?: string;
+    //readonly property
+    readonly city: string = 'New York';
+    
+    constructor(age: number, email?: string) {
+        this.age = age;
+        this.email = email;
+    }
+}
+
+const user = new User(30,"anne@dev.to") // this is an instance of the class
+
+user.name = "Anne";
+
+
+console.log(user);
+
+
 
